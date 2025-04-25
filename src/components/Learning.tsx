43d64d2afec6f76bc9a520038b22c9e1a6185236
Mohd Saalim <salimansari0986@gmail.com>
2025-04-25 15:43:33 +0530

    Update translation Api..🖊️

--- src/components/Learning.tsx
+++ src/components/Learning.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { generate } from "random-words";
 import axios from "axios";
